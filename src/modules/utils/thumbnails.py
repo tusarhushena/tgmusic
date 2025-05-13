39de333a63ed278a4cef52f938d29db409f53dc8
tosu <154593488+tusarhushena@users.noreply.github.com>
2025-05-13 09:56:40 +0530

    Update thumbnails.py

--- src/modules/utils/thumbnails.py
+++ src/modules/utils/thumbnails.py
@@ -185,7 +185,7 @@ async def gen_thumb(song: CachedTrack) -> str:
     bg.paste(image, (paste_x, paste_y), image)
 
     draw = ImageDraw.Draw(bg)
-    draw.text((285, 180), "Fallen Beatz", (192, 192, 192), font=FONTS["nfont"])
+    draw.text((285, 180), "Team DeadlineTech", (192, 192, 192), font=FONTS["nfont"])
     draw.text((285, 200), title, (255, 255, 255), font=FONTS["tfont"])
     draw.text((287, 235), artist, (255, 255, 255), font=FONTS["cfont"])
     draw.text((478, 321), get_duration(duration), (192, 192, 192), font=FONTS["dfont"])
