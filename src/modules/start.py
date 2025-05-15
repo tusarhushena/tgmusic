08bdc4d2f117ba8abff5c8e025cf3c0cb2aea8a2
tosu <154593488+tusarhushena@users.noreply.github.com>
2025-05-15 05:44:45 +0530

    Update start.py

--- src/modules/start.py
+++ src/modules/start.py
@@ -28,33 +28,6 @@ from src.modules.utils.strings import (
 )
 
 
-@Client.on_message(filters=Filter.command(["start", "help"]))
-async def start_cmd(c: Client, message: types.Message):
-    """
-    Handle the /start and /help command to welcome users.
-    """
-    chat_id = message.chat_id
-    bot_name = c.me.first_name
-    if chat_id < 0:
-        text = StartText.format(await message.mention(), bot_name, SUPPORT_GROUP)
-        reply = await message.reply_text(
-            text=text,
-            disable_web_page_preview=True,
-            reply_markup=SupportButton,
-        )
-        if isinstance(reply, types.Error):
-            c.logger.warning(f"Error sending start message: {reply.message}")
-        return None
-
-    text = PmStartText.format(await message.mention(), bot_name, __version__)
-    bot_username = c.me.usernames.editable_username
-    reply = await message.reply_text(text, reply_markup=add_me_markup(bot_username))
-    if isinstance(reply, types.Error):
-        c.logger.warning(f"Error sending start message: {reply.message}")
-
-    return None
-
-
 @Client.on_message(filters=Filter.command("privacy"))
 async def privacy_handler(c: Client, message: types.Message):
     """
